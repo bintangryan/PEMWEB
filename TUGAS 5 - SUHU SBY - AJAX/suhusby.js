80b101c4b776856c2b16c388587b59d95680d981
bintangryan <143849730+bintangryan@users.noreply.github.com>
2023-11-06 14:06:04 +0700

    Update suhusby.js

--- TUGAS 5 - SUHU SBY - AJAX/suhusby.js
+++ TUGAS 5 - SUHU SBY - AJAX/suhusby.js
@@ -1,26 +1,3 @@
-/*var apiKey = 'b761d568b0bc05cc3c5d9c7eca8e8832'; // Gantilah dengan API Key Anda
-var city = 'Surabaya';
-
-var xhr = new XMLHttpRequest();
-xhr.open('GET', `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`, true);
-
-xhr.onload = function () {
-    if (xhr.status === 200) {
-        var data = JSON.parse(xhr.responseText);
-
-        // Mengakses elemen HTML untuk menampilkan data cuaca
-        var cuacaElement = document.getElementById('suhu-sby');
-
-        // Menampilkan data cuaca di dalam elemen HTML
-        cuacaElement.innerHTML = `Cuaca di ${city}: ${data.weather[0].description}`;
-    } else {
-        console.error('Gagal mengambil data cuaca.');
-    }
-};
-
-xhr.send();*/
-
-
 var apiKey = 'b761d568b0bc05cc3c5d9c7eca8e8832'; // Gantilah dengan API Key Anda
 var city = 'SURABAYA';
 
